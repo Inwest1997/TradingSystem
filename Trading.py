a76499b92c1978aa93623427f7555ac447d0ceaf
Inwest1997 <jungis0402@gmail.com>
2022-10-17 14:45:01 +0900

    [2022-10-17]

---
+++ Trading.py
@@ -0,0 +1,20 @@
+from Index_Trend import *
+
+from strategy import *
+
+
+STRATEGY_DICT = {'rsi':rsi_strategy(rsi()),}
+
+
+class backtester:
+    def __init__(self, df, strategy_dict):
+        self.df = df
+    def strategy(self):
+        
+        pass 
+    def buy():
+        pass
+    def sell():
+        pass
+    
+        
