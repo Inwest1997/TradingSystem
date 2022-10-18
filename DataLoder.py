5cbf5166412a7802ec9781061e0ad8555f9628b6
Inwest1997 <jungis0402@gmail.com>
2022-10-18 12:36:27 +0900

    [2022-10-18] DataLoader 오류 수정

--- DataLoder.py
+++ DataLoder.py
@@ -17,18 +17,14 @@ DB = 'testdb'
 db_info = f'postgresql://{NAME}:{PW}@{USER_NAME}:{PORT}/{DB}'
 '''
 class DataGenerator:
-    def __init__(self, addr = ADDR ,file_name = FILE_NAME, sql = SQL, data_type = DATA_TYPE):
-        self.addr = addr
-        self.file_name = file_name
-        self.data_type = data_type
 
     def stock_data_generator(self, itv = INTERVAL, st_date = START_DATA , end_date = END_DATA, error_list = False):
         total_df_list = []
         error_stock = []
-        for stock in tqdm(STOCK_LIST['Symbol'][:10]):
+        for stock in tqdm(STOCK_LIST['Symbol']):
             try:
                 _ = yf.download(tickers = stock, start = st_date, end = end_date, interval = itv, progress=False, show_errors=False)
-                _.reset_index(inplace=True)
+
                 if len(_) == 0:
                     error_stock.append(stock)
                 else :
@@ -38,6 +34,7 @@ class DataGenerator:
             except:
                 print('ERROR!')
                 continue
+
         if error_list == True:
             return pd.concat(total_df_list), error_stock
         else :
@@ -67,46 +64,46 @@ class DataGenerator:
         except:
             print('DB 저장 실패')
     
-    def read_from_db():
+    def read_from_db(self):
         engine = sqlalchemy.create_engine(DB_INFO)
         conn = engine.connect()
         df = pd.read_sql(SQL, conn)
-        # df = df.set_index('Date')
         conn.close()
         return df
 
     def concat(self):
         new_data = self.stock_data_generator()
-        # new_data.reset_index(inplace=True)
+        new_data.reset_index(inplace = True)
+        new_data.index.name = 'Index'
+        
         try:
             if self.data_type == 'csv':
-                old_data = pd.read_csv(f'{os.path.join(self.addr, self.file_name)}.csv')
+                old_data = pd.read_csv(f'{os.path.join(ADDR, FILE_NAME)}.csv', index_col='index')
             elif self.data_type == 'db':
                 self.old_data = self.read_from_db()   
             print('SUCCESS!')
-            old_data.drop(['Unnamed: 0'], axis = 1, inplace = True)
 
         except:
-            if self.data_type == 'csv':
+            if DATA_TYPE == 'csv':
                 error_data = os.path.join(ADDR, FILE_NAME)+'.csv'
-            elif self.data_type == 'db':
+            elif DATA_TYPE == 'db':
                 error_data = TABLE_NAME
             print(f'NO SUCH DIRECTORY : {error_data}')
             old_data = pd.DataFrame(columns=new_data.columns)
             pass
-        # old_data.reset_index(inplace=True)
-        df = pd.concat([old_data, new_data], axis=0, ignore_index=True)
+        df = pd.concat([old_data, new_data], axis=0)
         df.drop_duplicates(keep = 'first', inplace=True)
-        df = df[new_data.columns]
         return df
 
     def upload(self) :
         df = self.concat()
-        if self.data_type == 'csv':
-            df.to_csv(f'./Dataset/{self.file_name}.csv')
+        if DATA_TYPE == 'csv':
+            df.to_csv(f'./Dataset/{FILE_NAME}.csv',index=False)
             pass
-        elif self.data_type == 'db':
+        elif DATA_TYPE == 'db':
             self.creat_table(TABLE_NAME)
         else:
             print('데이터 타입을 확인해주세요.')
+        # df['Datetime'] = df['Datetime'].apply(lambda x : dt.datetime.strptime(x[:-6], '%Y-%m-%d %H:%M:%S'))
+        return df
 
