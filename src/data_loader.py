55266daa4b59680627fb931746404f5958d8ccc2
Inwest1997 <jungis0402@gmail.com>
2022-11-02 17:35:19 +0900

    [2022-11-02] 오류 수정

--- src/data_loader.py
+++ src/data_loader.py
@@ -12,27 +12,22 @@ stock_list = pd.read_csv('./Dataset/exist_kis_nasdaq_list.csv')
 info = 'postgresql://junginseo:0000@localhost:5432/stock_db'
 
 class DataGenerator:
-    def __init__(self, data_type, interval = 'day', db_info = info) -> None:
+    def __init__(self, data_type, db_info = info, table_name = 'daily_stock') -> None:
         '''
         date_type : db or csv
-        interval: day or min
         '''
-        
+        self.table_name = table_name
         self.data_type = data_type
-        self.interval = interval
-        if self.interval == 'day':
-            self.table_name = 'daily_stock'
-        elif self.interval == 'min':
-            self.table_name = 'tick_stock'
         self.end_date = dt.datetime.today()
         self.db_info = db_info
 
     def read_from_csv(self, dir = './Dataset',ticker = None):
-        self.origin = pd.read_csv(f'{os.path.join(dir, self.table_name)}.csv', index_col=0)
+        origin = pd.read_csv(f'{os.path.join(dir, self.table_name)}.csv', index_col=0)
         if ticker != None:
             self.origin = self.origin[self.origin['Ticker']==ticker]
         self.dir = dir
         # print('파일에서 CSV 데이터 불러오기 성공')
+        return origin
 
 
     def read_from_db(self, sql = None, ticker = None):
@@ -44,17 +39,18 @@ class DataGenerator:
         if ticker != None:
             sql = f'''SELECT * FROM {self.table_name} WHERE "{self.table_name}"."Ticker" = '{ticker}';'''
 
-        self.origin = pd.read_sql(sql, conn)
-        self.origin['Datetime'] = self.origin['Datetime'].apply(lambda x : x.strftime('%Y-%m-%d')[:10])
-        # print('DB에서 데이터 불러오기 성공')
+        origin = pd.read_sql(sql, conn)
         conn.close()
+        origin['Datetime'] = origin['Datetime'].apply(lambda x : x.strftime('%Y-%m-%d')[:10])
+        return origin
 
 
     def read_origin_data(self, **kwargs):
         if self.data_type == 'csv':
-            self.read_from_csv(**kwargs)
+            origin = self.read_from_csv(**kwargs)
         elif self.data_type == 'db':
-            self.read_from_db(**kwargs)
+            origin = self.read_from_db(**kwargs)
+        return origin
 
     def date_gap(self):
         return (self.end_date - dt.datetime.strptime(self.origin.Datetime.max(), '%Y-%m-%d')).days
@@ -63,26 +59,18 @@ class DataGenerator:
 
     def stock_data_generator(self,  error_list = False, stock_list = stock_list, all = False) :
         total_df_list = []
-        error_stock = []
-        itv = '1'+self.interval[0]
+        self.error_stock = []
 
         if all == True:
-            if self.interval == 'day':
-                st_date = dt.datetime.strftime(self.end_date - dt.timedelta(weeks=52*30), '%Y-%m-%d')
-            elif self.interval == 'min':
-                st_date = dt.datetime.strftime(self.end_date - dt.timedelta(weeks=1), '%Y-%m-%d')
+            st_date = dt.datetime.strftime(self.end_date - dt.timedelta(weeks=52*30), '%Y-%m-%d')
         else:
-            if self.interval == 'min' and self.date_gap() > 7:
-                st_date = dt.datetime.strftime(self.end_date - dt.timedelta(weeks=1), '%Y-%m-%d')
-            elif self.interval == 'day':
-                st_date = dt.datetime.strftime(dt.datetime.strptime(self.origin.Datetime.max(),'%Y-%m-%d') + dt.timedelta(days=1), '%Y-%m-%d')
-            else:
-                st_date = self.origin.Datetime.max() 
+            st_date = dt.datetime.strftime(dt.datetime.strptime(self.origin.Datetime.max(),'%Y-%m-%d') + dt.timedelta(days=1), '%Y-%m-%d')
+
         for stock in tqdm(stock_list['Symbol']):
             try:
-                _ = yf.download(tickers = stock, start = st_date, end = self.end_date, interval = itv, progress=False, show_errors=False)
+                _ = yf.download(tickers = stock, start = st_date, end = self.end_date, interval = '1d', progress=False, show_errors=False)
                 if len(_) == 0:
-                    error_stock.append(stock)
+                    self.error_stock.append(stock)
                 else :
                     _.reset_index(inplace=True)
                     _.rename(columns={'Date':'Datetime'},inplace=True)
@@ -91,28 +79,27 @@ class DataGenerator:
             except:
                 print('ERROR!')
                 continue
-        self.new = pd.concat(total_df_list)
-        if error_list == True:
-            return error_stock
+        new = pd.concat(total_df_list)
+        return new        
         
 
     def data_concat(self) :
 
         try:
             if self.origin==None:
-                self.read_origin_data()
+                self.origin = self.read_origin_data()
             if self.new == None:
-                self.stock_data_generator()
+                self.new = self.stock_data_generator()
             print('SUCCESS!')
             
-            self.concat = pd.concat([self.origin, self.new], axis=0)
-            self.concat.reset_index(drop = True)
-            self.concat.drop_duplicates(keep = 'first', inplace=True)
-
+            concat = pd.concat([self.origin, self.new], axis=0)
+            concat.reset_index(drop = True)
+            concat.drop_duplicates(keep = 'first', inplace=True)
+            return concat
         except Exception as e:
-            # print(f'NO SUCH DIRECTORY OR TABLE')
             print('에러 발생 : ',e)
             pass
+        
 
 
 
@@ -120,67 +107,55 @@ class DataGenerator:
         try :
             engine = sqlalchemy.create_engine(self.db_info)
             conn = engine.connect()
-            # print('DB 연동 성공')
             self.new.to_sql(self.table_name, conn
                         , if_exists=  exist# ---append, replace
                         , index=False
                         , dtype={
-                                # "Datetime": sqlalchemy.DateTime,
-                                # "TickerName": String(),
+
                                 "Ticker": String(10)
-                                # ,"Open": BIGINT()
-                                # , "High": BIGINT()
-                                # , "Low": BIGINT()
-                                # , "Close": BIGINT()
-                                # , "Volume": BIGINT()
-                                # , 'Adj Close': BIGINT()
+      
                                 }
                         )
-            # print('DB 저장 완료')
 
             conn.close()
         except Exception as e:
             print(e, '로 인해 DB 저장 실패')
 
-    def upload(self) -> None:
+    def upload_to_csv(self):
+        concat = self.data_concat()
+        try:
+            concat.to_csv(f'./Dataset/{self.file}.csv',index=False)
+        except Exception as e:
+            print(e, '로 인해 CSV 저장 실패')
+
+
+    def update(self) -> None:
         try:
             if self.data_type == 'csv':
-                self.data_concat()
-                print('데이터 병합 성공')
-                self.concat.to_csv(f'./Dataset/{self.file}.csv',index=False)
+                self.upload_to_csv()
             elif self.data_type == 'db':
-                self.stock_data_generator()
-                self.creat_table()
-            else:
-                print('데이터 타입을 확인해주세요.')
-        except:
-            self.read_origin_data()
-            if self.data_type == 'csv':
-                self.data_concat()
-                print('데이터 병합 성공')
-                self.concat.to_csv(f'./Dataset/{self.file}.csv',index=False)
-            elif self.data_type == 'db':
-                self.stock_data_generator()
-                self.creat_table()
-            else:
-                print('데이터 타입을 확인해주세요.')
+                self.new = self.stock_data_generator()
+                self.upload_to_table()
+        except Exception as e:
+            print(e, '로 인해 업로드 실패')
+
 
     def data_search(self, ticker= None, stard_date=None, end_date=None):
-        self.read_origin_data(ticker = ticker)
-        self.origin['Datetime'] = to_datetime(datetime = self.origin['Datetime'],type = self.interval)
+        df = self.read_origin_data(ticker = ticker)
+        df['Datetime'] = to_datetime(datetime = df['Datetime'])
         if stard_date != None:
-            self.origin = self.origin[self.origin['Datetime']>=stard_date]
+            df = df[df['Datetime']>=stard_date]
         if end_date != None:
-            self.origin = self.origin[self.origin['Datetime']<=end_date]
-        return self.origin.set_index('Datetime').drop('Ticker', axis = 1).sort_index()
+            df = df[df['Datetime']<=end_date]
+        if ticker !=  None:
+            return df.set_index('Datetime').drop('Ticker', axis = 1).sort_index()
+        else:
+            return df
+            
 
 
 
 def to_datetime(datetime, type = 'min'):
-
-    if type == 'min':
-        datetime = datetime.apply(lambda x : dt.datetime.strptime(str(x)[:-6], '%Y-%m-%d %H:%M:%S'))
-    elif type == 'day':
-        datetime = datetime.apply(lambda x : dt.datetime.strptime(str(x), '%Y-%m-%d'))
+    datetime = datetime.apply(lambda x : dt.datetime.strptime(str(x), '%Y-%m-%d'))
     return datetime
 
