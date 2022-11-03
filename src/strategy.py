9281b8726c5118df19fee601e24e5e416622739b
unknown <115156803+guswns96@users.noreply.github.com>
2022-11-03 18:20:23 +0900

    Update strategy.py

--- src/strategy.py
+++ src/strategy.py
@@ -84,34 +84,32 @@ maintain signal : 0
 #     return df
 
 
-def rsi_strategy(df,up, down, **kwrgs):
+def rsi_strategy(df,up=70, down=30, **kwrgs):
     df = pd.concat([df,rsi(df, **kwrgs)], axis =1)
-    df['rsi_position'] = df.T.apply(lambda x : -1 if x['RSI'] > up else (1 if x['RSI'] < down else 0 ) )
-    return df
+    return df.apply(lambda x : -1 if x['RSI'] > up else (1 if x['RSI'] < down else 0 ) ,axis =1)
 
 def macd_strategy(df, **kwrgs):
     df = pd.concat([df,macd(df, **kwrgs)], axis =1)
-    df['macd_position'] = df.T.apply(lambda x : -1 if x['macd'] < x['macd_signal'] else (1 if x['macd'] > x['macd_signal'] else 0 ) )
-    return df
+    return df.apply(lambda x : -1 if x['macd'] < x['macd_signal'] else (1 if x['macd'] > x['macd_signal'] else 0 ),axis=1 )
 
 
 def envelope_strategy(df, **kwrgs):
     df = pd.concat([df,envelope(df, **kwrgs)], axis =1)
-    df['envelope_position'] = df.T.apply(lambda x : 1 if x['Adj Close'] < x['en_lb'] else (-1 if x['Adj Close'] > x['en_ub'] else 0 ) )
-    return df
+    return df.apply(lambda x : 1 if x['Adj Close'] < x['en_lb'] else (-1 if x['Adj Close'] > x['en_ub'] else 0 ) ,axis =1)
 
 
         
 def bollinger_strategy(df, **kwrgs):
     df = pd.concat([df,bollinger(df, **kwrgs)], axis =1)
-    df['bollinger_position'] = df.T.apply(lambda x : 1 if x['Adj Close'] < x['bo_lb'] else (-1 if x['Adj Close'] > x['bo_ub'] else 0 ) )
-    return df
+    return df.apply(lambda x : 1 if x['Adj Close'] < x['bo_lb'] else (-1 if x['Adj Close'] > x['bo_ub'] else 0 ) ,axis =1)
+
 
 
 def stochastic_strategy(df, **kwrgs):
     df = pd.concat([df,stochastic(df, **kwrgs)], axis =1)
-    df['stochastic_position'] = df.T.apply(lambda x : 1 if x['slow_k'] < x['slow_d'] else (-1 if x['slow_k'] > x['slow_d'] else 0 ) )
-    return df
+    return df.apply(lambda x : 1 if x['slow_k'] < x['slow_d'] else (-1 if x['slow_k'] > x['slow_d'] else 0 ) ,axis =1  )
+
+    
 
 
 
@@ -132,9 +130,13 @@ def buy_sum(df,ticker='GOOG',date= "2022-10-05",*args):
         print('매도')
 
 def test(df, *args):
-
-    args = [i+'_position' for i in args]
-    df['position'] = df[args].T.apply(lambda x :1 if np.array([i for i in x]).sum() == len(x) else  0)
+    for i in args:
+        exec(f"df['{i}_position'] = {i}_strategy(df)")
+    position_list = df.columns[-len(args):]
+    # [i+'_position' for i in args]
+    df['position'] = df[position_list].apply(lambda x :1 if np.array([i for i in x]).sum() == len(x) else  0, axis = 1)
+    
+    
     return df
 
 
