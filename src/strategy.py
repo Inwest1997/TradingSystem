1c88ceaf58930d87505e93168a706bab2882036b
Inwest1997 <jungis0402@gmail.com>
2022-11-04 14:25:47 +0900

    [2022-11-04] 전략 반환 변경

--- src/strategy.py
+++ src/strategy.py
@@ -6,28 +6,38 @@ import numpy as np
 
 def rsi_strategy(df,up=70, down=30, **kwrgs):
     df = pd.concat([df,rsi(df, **kwrgs)], axis =1)
-    return df.apply(lambda x : -1 if x['RSI'] > up else (1 if x['RSI'] < down else 0 ) ,axis =1)
+    position = [-1 if df.iloc[i]['RSI']>up else 1 if df.iloc[i]['RSI']<down else 0 for i in range(len(df))] 
+    return position
+    # df.apply(lambda x : -1 if x['RSI'] > up else (1 if x['RSI'] < down else 0 ) ,axis =1)
 
 def macd_strategy(df, **kwrgs):
     df = pd.concat([df,macd(df, **kwrgs)], axis =1)
-    return df.apply(lambda x : -1 if x['macd'] < x['macd_signal'] else (1 if x['macd'] > x['macd_signal'] else 0 ),axis=1 )
+    position = [-1 if df.iloc[i]['macd']<df.iloc[i]['macd_signal'] else 1 if df.iloc[i]['macd']>df.iloc[i]['macd_signal'] else 0 for i in range(len(df))] 
+    return position
+    # df.apply(lambda x : -1 if x['macd'] < x['macd_signal'] else (1 if x['macd'] > x['macd_signal'] else 0 ),axis=1 )
 
 
 def envelope_strategy(df, **kwrgs):
     df = pd.concat([df,envelope(df, **kwrgs)], axis =1)
-    return df.apply(lambda x : 1 if x['Adj Close'] < x['en_lb'] else (-1 if x['Adj Close'] > x['en_ub'] else 0 ) ,axis =1)
+    position = [-1 if df.iloc[i]['Adj Close'] > df.iloc[i]['en_ub'] else 1 if df.iloc[i]['Adj Close']<df.iloc[i]['en_lb'] else 0 for i in range(len(df))] 
+    return position
+    # df.apply(lambda x : 1 if x['Adj Close'] < x['en_lb'] else (-1 if x['Adj Close'] > x['en_ub'] else 0 ) ,axis =1)
 
 
         
 def bollinger_strategy(df, **kwrgs):
     df = pd.concat([df,bollinger(df, **kwrgs)], axis =1)
-    return df.apply(lambda x : 1 if x['Adj Close'] < x['bo_lb'] else (-1 if x['Adj Close'] > x['bo_ub'] else 0 ) ,axis =1)
+    position = [-1 if df.iloc[i]['Adj Close'] > df.iloc[i]['bo_ub'] else 1 if df.iloc[i]['Adj Close']<df.iloc[i]['bo_lb'] else 0 for i in range(len(df))] 
+    return position
+    # df.apply(lambda x : 1 if x['Adj Close'] < x['bo_lb'] else (-1 if x['Adj Close'] > x['bo_ub'] else 0 ) ,axis =1)
 
 
 
 def stochastic_strategy(df, **kwrgs):
     df = pd.concat([df,stochastic(df, **kwrgs)], axis =1)
-    return df.apply(lambda x : 1 if x['slow_k'] < x['slow_d'] else (-1 if x['slow_k'] > x['slow_d'] else 0 ) ,axis =1  )
+    position = [-1 if df.iloc[i]['slow_k'] > df.iloc[i]['slow_d'] else 1 if df.iloc[i]['slow_k']<df.iloc[i]['slow_d'] else 0 for i in range(len(df))] 
+    return position
+    df.apply(lambda x : 1 if x['slow_k'] < x['slow_d'] else (-1 if x['slow_k'] > x['slow_d'] else 0 ) ,axis =1  )
 
     
 
