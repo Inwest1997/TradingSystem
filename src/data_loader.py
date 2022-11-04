3037441c7be980a29bcf630d2a518ada95576ff2
Inwest1997 <jungis0402@gmail.com>
2022-11-04 14:25:18 +0900

    [2022-11-04] DataLoader 수정

--- src/data_loader.py
+++ src/data_loader.py
@@ -21,7 +21,7 @@ class DataGenerator:
         self.data_type = data_type
         self.end_date = dt.datetime.today()
         self.db_info = db_info
-        self.origin = pd.DataFrame()
+        self.origin = pd.DataFrame(columns=['Datetime','Open','High','Low','Close','Adj Close','Volume'])
 
     def read_from_csv(self,ticker = None):
         df = pd.read_csv(f'{os.path.join(self.dir, self.table_name)}.csv')
@@ -29,7 +29,7 @@ class DataGenerator:
             df = df[df['Ticker']==ticker]
         df['Datetime'] = pd.to_datetime(df['Datetime'])
         # print('파일에서 CSV 데이터 불러오기 성공')
-        self.origin = df
+        # self.origin = df
         return df
 
 
@@ -43,7 +43,7 @@ class DataGenerator:
             sql = f'''SELECT * FROM {self.table_name} WHERE "{self.table_name}"."Ticker" = '{ticker}';'''
 
         df = pd.read_sql(sql, conn)
-        self.origin = df 
+        # self.origin = df 
         conn.close()
         return df
 
@@ -53,6 +53,7 @@ class DataGenerator:
             df = self.read_from_csv(**kwargs)
         elif self.data_type == 'db':
             df = self.read_from_db(**kwargs)
+        print('기존 데이터 불러오기 성공')
         return df
 
     def date_gap(self):
@@ -83,22 +84,21 @@ class DataGenerator:
                 print('ERROR!')
                 continue
         new = pd.concat(total_df_list)
+        print('새로운 데이터 불러오기 성공')
         return new        
         
 
     def data_concat(self) :
-
         try:
-            if self.origin==None:
+            if self.origin.shape[0] == 0:
                 self.origin = self.read_origin_data()
-            if self.new == None:
-                self.new = self.stock_data_generator()
+            self.new = self.stock_data_generator()
             print('SUCCESS!')
             
-            concat = pd.concat([self.origin, self.new], axis=0)
-            concat.reset_index(drop = True)
-            concat.drop_duplicates(keep = 'first', inplace=True)
-            return concat
+            self.concat = pd.concat([self.origin, self.new], axis=0)
+            self.concat.reset_index(drop = True)
+            self.concat.drop_duplicates(keep = 'first', inplace=True)
+            return self.concat
         except Exception as e:
             print('에러 발생 : ',e)
             pass
@@ -125,40 +125,66 @@ class DataGenerator:
             print(e, '로 인해 DB 저장 실패')
 
     def upload_to_csv(self):
-        concat = self.data_concat()
         try:
-            concat.to_csv(f'./Dataset/{self.file}.csv',index=False)
+            self.concat.to_csv(f'./Dataset/{self.file}.csv',index=False)
         except Exception as e:
             print(e, '로 인해 CSV 저장 실패')
 
 
     def update(self) -> None:
+        self.data_concat()
         try:
             if self.data_type == 'csv':
                 self.upload_to_csv()
             elif self.data_type == 'db':
-                self.new = self.stock_data_generator()
                 self.upload_to_table()
         except Exception as e:
             print(e, '로 인해 업로드 실패')
 
 
-    def data_search(self, ticker= None, stard_date=None, end_date=None):
-        if self.origin.shape[0] == 0:
-            df = self.read_origin_data()
-        else:
-            df = self.origin
-        if stard_date != None:
-            df = df[df['Datetime']>=stard_date]
-        if end_date != None:
-            df = df[df['Datetime']<=end_date]
-        if ticker !=  None:
-            df = df[df['Ticker']==ticker]
-            return df.drop('Ticker',axis=1).set_index('Datetime')
-        else:
-            print('종목을 입력해주세요')
-            pass
+    # def data_search(self, ticker= None, stard_date=None, end_date=None, all = False):
+    #     try:
+    #         if self.origin.shape[0] == 0 and ticker == None:
+    #             df = self.read_origin_data()
+    #             return df
+    #         elif self.origin.shape[0] == 0 and ticker != None:
+    #             df = 
+    #         elif ticker != None:
+    #             df = self.read_origin_data(ticker = ticker)
+    #             if stard_date != None:
+    #                 df = df[df['Datetime']>=stard_date]
+    #             if end_date != None:
+    #                 df = df[df['Datetime']<=end_date]
+    #             return df.drop('Ticker',axis=1).set_index('Datetime')
+    #     except Exception as e:
+    #         print(e, '로 인해 데이터를 불러오지 못 했습니다.')
+
+
+    def data_search(self, ticker= None, stard_date=None, end_date=None, all = False):
+        try:
+            if self.origin.shape[0] == 0:
+                if  ticker != None:
+                    df = self.read_origin_data(ticker = ticker)
+                else:
+                    df = self.read_origin_data()
+            elif self.origin.shape[0] > 0:
+                if  ticker != None:
+                    df = self.origin[self.origin['Ticker']==ticker]
+                else:
+                    df = self.origin
+            if stard_date != None:
+                df = df[df['Datetime']>=stard_date]
+            if end_date != None:
+                df = df[df['Datetime']<=end_date]
             
+            if all == False:
+                return df.drop('Ticker',axis=1).set_index('Datetime')
+            else:
+                return df
+        except Exception as e:
+            print(e, '로 인해 데이터를 불러오지 못 했습니다.')
+            
+
             
 
 
